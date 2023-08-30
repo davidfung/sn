06085bf8beee6bb8bac6d7cc1373b85d860cca5f
davidfung <davidfung@amgcomputing.com>
2023-08-29 20:43:34 -0700

    Add command line option specifying which episode to show

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use std::env;
 use std::io;
 use webbrowser;
 
@@ -9,7 +10,7 @@ fn main() {
     display_show_notes(episode);
 }
 
-fn current_episode() -> i64 {
+fn get_current_episode() -> i64 {
     let base_episode_no = 895;
     let base_episode_date = Local.ymd(2022, 11, 2).and_hms(0, 0, 0);
     let today = Local::now();
@@ -27,8 +28,28 @@ fn display_show_notes(episode: i64) {
 }
 
 fn get_episode() -> i64 {
-    let mut episode;
-    let current_episode = current_episode();
+    let current_episode = get_current_episode();
+    let mut episode = get_cli_episode();
+    if episode == 0 {
+        episode = ask_episode(current_episode);
+    }
+    if episode < 0 {
+        episode = current_episode + episode;
+    }
+    episode
+}
+
+fn get_cli_episode() -> i64 {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        0
+    } else {
+        args[1].trim().parse().expect("Expect a number.")
+    }
+}
+
+fn ask_episode(current_episode: i64) -> i64 {
+    let episode;
     println!("Enter Security Now episode no. [{}]> ", current_episode);
 
     let mut s = String::new();
@@ -38,9 +59,6 @@ fn get_episode() -> i64 {
         episode = current_episode;
     } else {
         episode = s.trim().parse().expect("Expect a number.");
-        if episode < 0 {
-            episode = current_episode + episode;
-        }
     }
     episode
 }
